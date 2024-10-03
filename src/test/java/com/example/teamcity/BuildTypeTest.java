32ab9ce8c500baaf04c0e860485ac402fcd6830d
aKuslin123 <a.Kuslin123@gmail.com>
2024-10-03 23:54:51 +0700

    Added projectAdminCreatesBuildTypeTest, projectAdminCreatesBuildTypeForAnotherUserProjectTest

--- src/test/java/com/example/teamcity/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/BuildTypeTest.java
@@ -1,7 +1,6 @@
 package com.example.teamcity;
 
-import com.example.teamcity.api.models.BuildType;
-import com.example.teamcity.api.models.Project;
+import com.example.teamcity.api.models.*;
 import com.example.teamcity.api.requests.CheckedRequests;
 import com.example.teamcity.api.requests.unchecked.UncheckedBase;
 import com.example.teamcity.api.spec.Specifications;
@@ -53,11 +52,54 @@ public class BuildTypeTest extends BaseApiTest {
         step("Create project");
         step("Grant user PROJECT_ADMIN role in project");
         step("Create buildType for project by user PROJECT_ADMIN");
+
+        superUserCheckRequests.getRequest(PROJECTS).create(testData.getProject());
+
+        testData.getUser().setRoles(generate(Roles.class, "PROJECT_ADMIN", "p:" + testData.getProject().getId()));
+
+        superUserCheckRequests.<User>getRequest(USERS).create(testData.getUser());
+        var userCheckRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+
+        userCheckRequests.getRequest(BUILD_TYPES).create(testData.getBuildType());
+
         step("Check buildType was created successfully");
+        //проверяем просто что создан успешно, потому что тут тест на права и роли
+        //https://www.youtube.com/watch?v=NeH_WEWP-VI&ab_channel=AlexPshe 16:00
     }
 
     @Test(description = "Project admin should not be able to create build type for not their project", groups = {"Negative", "Roles"})
     public void projectAdminCreatesBuildTypeForAnotherUserProjectTest() {
+        var user1 = testData.getUser();
+        var project1 = testData.getProject();
+        System.out.println("user1 name = " + user1.getUsername());
+        System.out.println("project1 name = " + project1.getName());
+
+        superUserCheckRequests.getRequest(PROJECTS).create(project1);
+
+        user1.setRoles(generate(Roles.class, "PROJECT_ADMIN", "p:" + project1.getId()));
+
+        superUserCheckRequests.<User>getRequest(USERS).create(user1);
+        //мне вообще тут не нужно делать билдтайпы, только в конце вторым юзером, создать билдтайп для 1 юзера
+        //var userCheckRequests = new CheckedRequests(Specifications.authSpec(user1));
+        //userCheckRequests.getRequest(BUILD_TYPES).create(testData.getBuildType());
+
+        var testData2 = generate();
+        var user2 = testData2.getUser();
+        var project2 = testData2.getProject();
+        System.out.println("user2 name = " + user2.getUsername());
+        System.out.println("project2 name = " + project2.getName());
+
+        superUserCheckRequests.getRequest(PROJECTS).create(project2);
+
+        user2.setRoles(generate(Roles.class, "PROJECT_ADMIN", "p:" + project2.getId()));
+
+        superUserCheckRequests.<User>getRequest(USERS).create(user2);
+        var user2CheckRequests = new CheckedRequests(Specifications.authSpec(user2));
+
+        user2CheckRequests.getRequest(BUILD_TYPES).create(testData.getBuildType());
+
+
+
         step("Create user1");
         step("Create project1");
         step("Grant user1 PROJECT_ADMIN role in project1");
